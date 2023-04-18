04fdec64ccaf6c3a5e94d70c5aac71d058653db1
y9yk <andrew.yk82@gmail.com>
2023-04-18 23:16:56 +0900

    add base repository

---
+++ src/common/repositories/repository.ts
@@ -0,0 +1,65 @@
+import {
+  FilterQuery,
+  Model,
+  QueryOptions,
+  SaveOptions,
+  UpdateQuery,
+  UpdateWithAggregationPipeline,
+} from 'mongoose';
+import { Document } from 'mongoose';
+import { CreatedModel, RemovedModel, UpdatedModel } from './entity';
+
+export class Repository<T extends Document> {
+  constructor(private readonly model: Model<T>) {}
+
+  async create(doc: object, saveOptions?: SaveOptions): Promise<CreatedModel> {
+    const createdEntity = new this.model(doc);
+    const savedResult = await createdEntity.save(saveOptions);
+    // return id and created flag
+    return { id: savedResult.id, created: !!savedResult.id };
+  }
+
+  async find(filter: FilterQuery<T>, options?: QueryOptions): Promise<T[]> {
+    return await this.model.find(filter, null, options);
+  }
+
+  async findById(id: string): Promise<T | null> {
+    return await this.model.findById(id);
+  }
+
+  async findAll(): Promise<T[]> {
+    return await this.model.find();
+  }
+
+  async deleteOne(
+    filter: FilterQuery<T>,
+    options?: QueryOptions,
+  ): Promise<RemovedModel> {
+    const { deletedCount } = await this.model.deleteOne(filter, options);
+    return { deletedCount, deleted: !!deletedCount };
+  }
+
+  async deleteMany(
+    filter: FilterQuery<T>,
+    options?: QueryOptions,
+  ): Promise<RemovedModel> {
+    const { deletedCount } = await this.model.deleteMany(filter, options);
+    return { deletedCount, deleted: !!deletedCount };
+  }
+
+  async updateOne(
+    filter: FilterQuery<T>,
+    updated: UpdateWithAggregationPipeline | UpdateQuery<T>,
+    options?: QueryOptions,
+  ): Promise<UpdatedModel> {
+    return await this.model.updateOne(filter, updated, options);
+  }
+
+  async updateMany(
+    filter: FilterQuery<T>,
+    updated: UpdateWithAggregationPipeline | UpdateQuery<T>,
+    options?: QueryOptions,
+  ): Promise<UpdatedModel> {
+    return await this.model.updateMany(filter, updated, options);
+  }
+}
