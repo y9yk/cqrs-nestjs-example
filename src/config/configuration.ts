9fb048a7a8c3388efcc767b1991608717e6d6e12
y9yk <andrew.yk82@gmail.com>
2023-04-18 16:03:39 +0900

    add config module

---
+++ src/config/configuration.ts
@@ -0,0 +1,45 @@
+const getMongoConnectionUrl = (
+  host: string,
+  port: number,
+  username: string,
+  password: string,
+  database: string,
+) => {
+  return `mongodb://${username}:${password}@${host}:${port}/${database}?authSource=admin`;
+};
+
+export const configuration = () => ({
+  env: process.env.NODE_ENV,
+  service: {
+    name: process.env.SERVICE_NAME,
+    port: process.env.SERVICE_PORT,
+    keep_alive_timeout: process.env.SERVICE_KEEP_ALIVE_TIMEOUT,
+    log_level: process.env.LOG_LEVEL,
+  },
+  db: {
+    service: {
+      uri: getMongoConnectionUrl(
+        process.env.MONGO_SERVICE_HOST,
+        parseInt(process.env.MONGO_SERVICE_PORT),
+        process.env.MONGO_SERVICE_USERNAME,
+        process.env.MONGO_SERVICE_PASSWORD,
+        process.env.MONGO_SERVICE_DATABASE,
+      ),
+    },
+    eventstore: {
+      uri: getMongoConnectionUrl(
+        process.env.MONGO_EVENTSTORE_HOST,
+        parseInt(process.env.MONGO_EVENTSTORE_PORT),
+        process.env.MONGO_EVENTSTORE_USERNAME,
+        process.env.MONGO_EVENTSTORE_PASSWORD,
+        process.env.MONGO_EVENTSTORE_DATABASE,
+      ),
+    },
+  },
+  security: {
+    jwt: {
+      secret: process.env.JWT_SECRET,
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    },
+  },
+});
---
+++ src/config/validation.ts
@@ -0,0 +1,31 @@
+import * as Joi from 'joi';
+
+export const validationScheme = Joi.object({
+  NODE_ENV: Joi.string().valid(
+    'development',
+    'production',
+    'test',
+    'provision',
+  ),
+  // service
+  SERVICE_NAME: Joi.string().default('event-sourcing-cqrs-nestjs-example'),
+  SERVICE_PORT: Joi.number().default(8080),
+  SERVICE_KEEP_ALIVE_TIMEOUT: Joi.number().default(620),
+  // db (service)
+  MONGO_SERVICE_HOST: Joi.string().default('127.0.0.1'),
+  MONGO_SERVICE_PORT: Joi.number().default(27017),
+  MONGO_SERVICE_USERNAME: Joi.string().required(),
+  MONGO_SERVICE_PASSWORD: Joi.string().required(),
+  MONGO_SERVICE_DATABASE: Joi.string().default('service'),
+  // db (eventstore)
+  MONGO_EVENTSTORE_HOST: Joi.string().default('127.0.0.1'),
+  MONGO_EVENTSTORE_PORT: Joi.number().default(27018),
+  MONGO_EVENTSTORE_USERNAME: Joi.string().required(),
+  MONGO_EVENTSTORE_PASSWORD: Joi.string().required(),
+  MONGO_EVENTSTORE_DATABASE: Joi.string().default('eventstore'),
+  // security - jwt
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPIRES_IN: Joi.number().default(1 * 60 * 60),
+  // log
+  LOG_LEVEL: Joi.string().default('info'),
+});
