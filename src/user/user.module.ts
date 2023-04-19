c02291310ed91c056288a3fcf24c6a2e3e3e500a
y9yk <andrew.yk82@gmail.com>
2023-04-19 12:18:34 +0900

    add user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+import { UserRepository } from './repositories/user.repository';
+import { QueryHandlers } from './queries/handlers';
+import { CqrsModule } from '@nestjs/cqrs';
+import { SERVICE_DB_CONNECTION_NAME } from 'src/common/constants';
 
-@Module({})
+@Module({
+  imports: [
+    CqrsModule,
+    MongooseModule.forFeature(
+      [{ name: User.name, schema: UserSchema }],
+      SERVICE_DB_CONNECTION_NAME,
+    ),
+  ],
+  controllers: [UserController],
+  providers: [...QueryHandlers, UserRepository],
+})
 export class UserModule {}
