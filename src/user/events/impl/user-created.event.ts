f59738d776961939d4b7180bdf24cc9e71c40253
y9yk <andrew.yk82@gmail.com>
2023-04-19 23:56:34 +0900

    not use storable-event class

--- src/user/events/impl/user-created.event.ts
+++ src/user/events/impl/user-created.event.ts
@@ -1,16 +1,7 @@
-import { StorableEvent } from 'event-sourcing-nestjs';
-
-export class UserCreatedEvent extends StorableEvent {
-  eventAggregate = 'user';
-  eventVersion = 1;
-
+export class UserCreatedEvent {
   constructor(
-    // this id is an event id (not user's id)
-    public readonly id: string,
     public readonly name: string,
     public readonly email: string,
     public readonly password: string,
-  ) {
-    super();
-  }
+  ) {}
 }
