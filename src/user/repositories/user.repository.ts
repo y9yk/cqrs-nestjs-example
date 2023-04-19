3f50fc58c2cfad69fac5a41ef327c173be76b4c1
y9yk <andrew.yk82@gmail.com>
2023-04-19 12:18:13 +0900

    add user repository

--- src/user/repositories/user.repository.ts
+++ src/user/repositories/user.repository.ts
@@ -3,11 +3,15 @@ import { Repository } from 'src/common/repositories/repository';
 import { User } from '../schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { SERVICE_DB_CONNECTION_NAME } from 'src/common/constants';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   // inject model to repository class
-  constructor(@InjectModel(User.name) private entity: Model<User>) {
+  constructor(
+    @InjectModel(User.name, SERVICE_DB_CONNECTION_NAME)
+    private entity: Model<User>,
+  ) {
     super(entity);
   }
 }
