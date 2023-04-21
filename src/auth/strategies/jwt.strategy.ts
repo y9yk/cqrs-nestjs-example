a1070b24a2d6c2b51a7354af1fddbd4141eee4fe
y9yk <andrew.yk82@gmail.com>
2023-04-21 15:45:39 +0900

    add strategy module

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('security.jwt.secret'),
+    });
+  }
+
+  async validate(payload: any) {
+    // this information will be attached Request(object)
+    return { name: payload.name, email: payload.email };
+  }
+}
