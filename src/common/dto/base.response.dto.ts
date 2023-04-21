af3e3285cd479e3a2ea8c96a37ca662e192732d6
y9yk <andrew.yk82@gmail.com>
2023-04-21 15:43:01 +0900

    update base response dto

--- src/common/dto/base.response.dto.ts
+++ src/common/dto/base.response.dto.ts
@@ -20,3 +20,61 @@ export class ResponseDto {
   @IsOptional()
   results?: any;
 }
+
+export class HttpExceptionResponseDto {
+  @ApiProperty({
+    required: true,
+  })
+  statusCode: number;
+
+  @ApiProperty({
+    required: true,
+  })
+  message: string;
+
+  @ApiProperty({
+    required: false,
+  })
+  @IsOptional()
+  error?: any;
+}
+
+export class BadRequesExceptionResponseDto extends HttpExceptionResponseDto {
+  @ApiProperty({
+    example: 400,
+    required: true,
+  })
+  statusCode: number;
+}
+
+export class UnAuthorizedExceptionResponseDto extends HttpExceptionResponseDto {
+  @ApiProperty({
+    example: 401,
+    required: true,
+  })
+  statusCode: number;
+}
+
+export class NotFoundExceptionResponseDto extends HttpExceptionResponseDto {
+  @ApiProperty({
+    example: 404,
+    required: true,
+  })
+  statusCode: number;
+}
+
+export class ConflictExceptionResponseDto extends HttpExceptionResponseDto {
+  @ApiProperty({
+    example: 409,
+    required: true,
+  })
+  statusCode: number;
+}
+
+export class InternalServerErrorExceptionResponseDto extends HttpExceptionResponseDto {
+  @ApiProperty({
+    example: 500,
+    required: true,
+  })
+  statusCode: number;
+}
