68e60dd15e54d8e70c8981c9249983800c4cd404
y9yk <andrew.yk82@gmail.com>
2023-04-21 22:34:18 +0900

    update controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,41 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
-import { GetUserByEmailQuery, GetUsersQuery } from './queries/impl';
-import { User } from './schemas/user.schema';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { CreateUserDto } from './dto/user.request.dto';
+import { GetUsersQuery } from './queries/impl';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import {
+  CreateUserRequestDto,
+  GetUsersRequestDto,
+} from './dto/user.request.dto';
 import { CreateUserCommand } from './commands/impl/create-user.command';
+import { JwtAuthGuard } from 'src/auth/guards/auth.guard';
+import { GetUsersResponseDto } from './dto/user.response.dto';
+import {
+  BadRequesExceptionResponseDto,
+  InternalServerErrorExceptionResponseDto,
+  UnAuthorizedExceptionResponseDto,
+} from 'src/common/dto/base.response.dto';
 
 @ApiTags('User Service')
+@ApiBadRequestResponse({ type: BadRequesExceptionResponseDto })
+@ApiInternalServerErrorResponse({
+  type: InternalServerErrorExceptionResponseDto,
+})
 @Controller('user')
 export class UserController {
   constructor(
@@ -15,22 +44,39 @@ export class UserController {
   ) {}
 
   @ApiOperation({ summary: '사용자 생성' })
-  @Post()
-  async create(@Body() dto: CreateUserDto) {
+  @ApiResponse({
+    status: 200,
+  })
+  @HttpCode(200)
+  @Post('/create')
+  async create(@Body() dto: CreateUserRequestDto) {
     return this.commandBus.execute(
       new CreateUserCommand(dto.name, dto.email, dto.password),
     );
   }
 
   @ApiOperation({ summary: '사용자 조회' })
-  @Get()
-  async getAll(): Promise<User> {
-    return this.queryBus.execute(new GetUsersQuery());
-  }
-
-  @ApiOperation({ summary: '사용자 조회(이메일)' })
-  @Get('/email/:email')
-  async getByEmail(@Param('email') email: string): Promise<User> {
-    return this.queryBus.execute(new GetUserByEmailQuery(email));
+  @ApiBearerAuth('AccessToken')
+  @ApiResponse({
+    status: 200,
+    type: GetUsersResponseDto,
+  })
+  @ApiUnauthorizedResponse({ type: UnAuthorizedExceptionResponseDto })
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(200)
+  @Get('/get')
+  async getUsers(
+    @Query() dto: GetUsersRequestDto,
+  ): Promise<GetUsersResponseDto> {
+    return this.queryBus.execute(
+      new GetUsersQuery(
+        dto.name,
+        dto.email,
+        dto.page,
+        dto.size,
+        dto.sortField,
+        dto.sortOrder,
+      ),
+    );
   }
 }
