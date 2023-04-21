522d3c1e5b74d949e237aa86a1b1525232399f13
y9yk <andrew.yk82@gmail.com>
2023-04-21 15:46:25 +0900

    override toString

--- src/user/events/impl/user-created.event.ts
+++ src/user/events/impl/user-created.event.ts
@@ -2,6 +2,16 @@ export class UserCreatedEvent {
   constructor(
     public readonly name: string,
     public readonly email: string,
-    public readonly password: string,
+    public readonly dispatchedTime: string,
   ) {}
+
+  public toString(): string {
+    const plainObject = {
+      name: this.name,
+      email: this.email,
+      dispatchedTime: this.dispatchedTime,
+    };
+    // stringify and return
+    return JSON.stringify(plainObject);
+  }
 }
