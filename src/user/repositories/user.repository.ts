430902d1d090744d295a0922dd797ffc54c025bc
y9yk <andrew.yk82@gmail.com>
2023-04-21 15:46:12 +0900

    relocate schema

--- src/user/repositories/user.repository.ts
+++ src/user/repositories/user.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'src/common/repositories/repository';
-import { User } from '../schemas/user.schema';
+import { User } from '../../common/schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { SERVICE_DB_CONNECTION_NAME } from 'src/common/constants';
