a090e73e535bc3c5c3829aca06eb286f2a18fb1f
y9yk <andrew.yk82@gmail.com>
2023-04-21 15:45:56 +0900

    add auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { LoginRequestDto } from './dto/login.request.dto';
+import { CommandBus } from '@nestjs/cqrs';
+import { LoginCommand } from './commands/impl/login.command';
+import { LoginResponseDto } from './dto/login.response.dto';
+import {
+  BadRequesExceptionResponseDto,
+  InternalServerErrorExceptionResponseDto,
+  UnAuthorizedExceptionResponseDto,
+} from 'src/common/dto/base.response.dto';
+
+@ApiTags('Auth Service')
+@Controller('auth')
+@ApiBadRequestResponse({ type: BadRequesExceptionResponseDto })
+@ApiInternalServerErrorResponse({
+  type: InternalServerErrorExceptionResponseDto,
+})
+export class AuthController {
+  constructor(private readonly commandBus: CommandBus) {}
+
+  @ApiOperation({ summary: '사용자 로그인' })
+  @ApiResponse({
+    status: 200,
+    type: LoginResponseDto,
+  })
+  @ApiUnauthorizedResponse({ type: UnAuthorizedExceptionResponseDto })
+  @HttpCode(200)
+  @Post('/login')
+  public async login(@Body() dto: LoginRequestDto): Promise<LoginResponseDto> {
+    return this.commandBus.execute(new LoginCommand(dto.email, dto.password));
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,45 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './services/auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { AuthRepository } from './repositories/auth.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/common/schemas/user.schema';
+import { SERVICE_DB_CONNECTION_NAME } from 'src/common/constants';
+import { AuthController } from './auth.controller';
+import { CqrsModule } from '@nestjs/cqrs';
+import { CommandHandlers } from './commands/handlers';
+import { EventHandlers } from './events/handlers';
 
-@Module({})
+@Module({
+  imports: [
+    CqrsModule,
+    PassportModule,
+    MongooseModule.forFeature(
+      [{ name: User.name, schema: UserSchema }],
+      SERVICE_DB_CONNECTION_NAME,
+    ),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('security.jwt.secret'),
+        signOptions: {
+          expiresIn: configService.get<number>('security.jwt.expiresIn'),
+        },
+      }),
+    }),
+  ],
+  providers: [
+    ...CommandHandlers,
+    ...EventHandlers,
+    // service and strategy
+    AuthService,
+    JwtStrategy,
+    // repostiroy
+    AuthRepository,
+  ],
+  controllers: [AuthController],
+})
 export class AuthModule {}
