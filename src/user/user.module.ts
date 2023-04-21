ac69813724aadcff2e85af58d9932da44ce71bd0
y9yk <andrew.yk82@gmail.com>
2023-04-21 22:16:49 +0900

    register user service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schemas/user.schema';
+import { User, UserSchema } from '../common/schemas/user.schema';
 import { UserRepository } from './repositories/user.repository';
 import { QueryHandlers } from './queries/handlers';
 import { CqrsModule } from '@nestjs/cqrs';
 import { SERVICE_DB_CONNECTION_NAME } from 'src/common/constants';
 import { CommandHandlers } from './commands/handlers';
 import { EventHandlers } from './events/handlers';
+import { UserService } from './services/user.service';
 
 @Module({
   imports: [
@@ -22,6 +23,9 @@ import { EventHandlers } from './events/handlers';
     ...CommandHandlers,
     ...QueryHandlers,
     ...EventHandlers,
+    // service
+    UserService,
+    // repository
     UserRepository,
   ],
 })
